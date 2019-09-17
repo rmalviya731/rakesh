194c0c0d09d23f91eacfa21419f719b8dbb55f57
rmalviya731 <53622170+rmalviya731@users.noreply.github.com>
2019-09-17 12:03:40 +0530

    Update awss3.sh

--- awss3.sh
+++ awss3.sh
@@ -1,6 +1,6 @@
 aws --version
-export AWS_ACCESS_KEY_ID=AKIAQJ4ZMLJU5OA2ZAK7
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_
+export AWS_SECRET_ACCESS_KEY=
 #aws s3api create-bucket --bucket my-bucket-rakesh --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 #aws s3api create-bucket --bucket my-bucket-demo --region ap-south-1
 aws s3 mb s3://rakeshdemo3 --region ap-south-1
